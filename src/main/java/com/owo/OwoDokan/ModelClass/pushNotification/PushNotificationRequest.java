67a5cfc94cea3a388e15c5dc4f1e0bb29f9e09c7
Amimul <amimulahsan7@gmail.com>
2021-04-11 02:10:49 +0600

    Added push notification logics.

---
+++ src/main/java/com/owo/OwoDokan/ModelClass/pushNotification/PushNotificationRequest.java
@@ -0,0 +1,14 @@
+package com.owo.OwoDokan.ModelClass.pushNotification;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@Setter
+@Getter
+@NoArgsConstructor
+public class PushNotificationRequest {
+    private String title;
+    private String message;
+    private String topic;
+    private String token;
+}
