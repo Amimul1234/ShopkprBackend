a79ad35e2c91c7cd9e1c79e9b731f71db7a8bc52
Amimul <amimulahsan7@gmail.com>
2021-05-05 22:03:31 +0600

    Need to add product expire date.

--- src/main/java/com/shopKpr/entity/product/OwoProduct.java
+++ src/main/java/com/shopKpr/entity/product/OwoProduct.java
@@ -37,7 +37,6 @@ public class OwoProduct implements Serializable {
     private String productCreationDate;
     @Column(nullable = false)
     private String productCreationTime;
-    @Column(nullable = false)
     private String productExpireDate;
     @Column(nullable = false, columnDefinition = "LONGTEXT")
     private String productImage;
